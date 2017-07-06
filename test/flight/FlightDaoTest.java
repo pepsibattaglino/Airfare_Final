c81edc1c74e6b33801eb2be120497c3b03390806
Giuseppe Battaglino <pepsibattaglino@gmail.com>
2017-07-05 22:15:44 -0300

    Completing FlightDaoTest

--- test/flight/FlightDaoTest.java
+++ test/flight/FlightDaoTest.java
@@ -38,56 +38,27 @@ public class FlightDaoTest {
 
     @Test
     public void updateFlights() throws Exception {
+        FlightDao dao = new FlightDao();
+        Flight f = new Flight("Foggia", "Los Angeles", LocalDateTime.now(), 1);
+
+        if(dao.updateFlights(f)){
+            System.out.println("Flight updated with success.");
+        } else {
+            fail("Failed to update a Flight.");
+        }
     }
 
     @Test
     public void deleteFlights() throws Exception {
-    }
+        FlightDao dao = new FlightDao();
+        Flight f = new Flight();
+        f.setFlightID(3);
 
-/*    @Test
-    void createAirplane() throws Exception {
-        Airplane airplane = new Airplane("IPX-7432", "AirFit", 5);
-        AirplaneDao dao = new AirplaneDao();
-
-        if(dao.createAirplane(airplane)) {
-            System.out.println("Plane created with success.");
+        if(dao.deleteFlights(f)){
+            System.out.println("Flight deleted with success.");
         } else {
-            fail("Failed to create a plane.");
+            fail("Failed to delete a Flight.");
         }
     }
-
-    @Test
-    void listAllAirplanes() {
-        AirplaneDao dao = new AirplaneDao();
-
-        for(Airplane a: dao.listAllAirplanes()){
-            System.out.println("Plane model: " + a.getModel());
-        }
-    }
-
-    @Test
-    void updateAirplane() {
-        AirplaneDao dao = new AirplaneDao();
-        Airplane a = new Airplane(99, "DSS-6593", "Songbird", 5);
-
-        if(dao.updateAirplane(a)){
-            System.out.println("Plane updated with success.");
-        } else {
-            fail("Failed to update a plane.");
-        }
-    }
-
-    @Test
-    void deleteAirplane() {
-        AirplaneDao dao = new AirplaneDao();
-        Airplane a = new Airplane();
-        a.setAirplaneID(3);
-
-        if(dao.deleteAirplane(a)){
-            System.out.println("Plane deleted with success.");
-        } else {
-            fail("Failed to delete a plane.");
-        }
-    }*/
 
 }
