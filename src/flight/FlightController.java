5db150efe98bface2f23b4f9ed976ee8984c2f93
Giuseppe Battaglino <pepsibattaglino@gmail.com>
2017-07-08 16:49:24 -0300

    Fix Flight
    implement Controller, business and more

---
+++ src/flight/FlightController.java
@@ -0,0 +1,108 @@
+package flight;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.VBox;
+
+import javax.swing.*;
+import java.time.LocalDate;
+
+/**
+ * Created by G.Battaglino on 08/07/2017.
+ */
+public class FlightController {
+
+   /* @FXML
+    private VBox formFlight;*/
+
+    @FXML
+    private TextField flightOriginField;
+
+    @FXML
+    private TextField flightDestinationField;
+
+    @FXML
+    private DatePicker flightDepartureField;
+
+    @FXML
+    private TextField flightDesignatedAirplaneField;
+
+    @FXML
+    private TextField flightAvaliableSeatsField;
+
+    /*@FXML
+    private Button btnClear;
+
+    @FXML
+    private Button btnSave;*/
+
+    @FXML
+    void treatClearButton(ActionEvent event) {
+        System.out.println("The Clear button has been pressed.");
+        clearFields();
+    }
+
+    @FXML
+    void treatSaveButton(ActionEvent event) {
+        System.out.println("The Save button has been pressed.");
+        if (
+                notEmptyField(flightOriginField) &&
+                notEmptyField(flightDestinationField) &&
+                notEmptyField(flightDepartureField) &&
+                notEmptyField(flightDesignatedAirplaneField) &&
+                notEmptyField(flightAvaliableSeatsField)) {
+
+            FlightBusiness fb = new FlightBusiness();
+            if (
+                    fb.flightOriginChecker(flightOriginField.getText()) &&
+                    fb.flightDestinationChecker(flightDestinationField.getText()) &&
+                    fb.flightDepartureChecker(flightDepartureField.getText()) &&
+                    fb.flightDesignatedAirplaneChecker(flightDesignatedAirplaneField.getText()) &&
+                    fb.flightAvailableSeatsChecker(flightAvaliableSeatsField.getText())) {
+
+                registerFlight(flightOriginField, flightDestinationField, flightDepartureField, flightDesignatedAirplaneField, flightAvaliableSeatsField);
+                clearFields();
+                JOptionPane.showMessageDialog(null, "Flight saved with success!");
+            } else {
+                System.out.println("Failed to verify the flight.");
+            }
+        } else {
+            JOptionPane.showMessageDialog(null,
+                    "One or more required fields are empty.",
+                    "Error",
+                    JOptionPane.ERROR_MESSAGE);
+        }
+
+    }
+
+    private void registerFlight (TextField origin, TextField destination, DatePicker departure, TextField designatedAirplane, TextField availableSeats) {
+        Flight f = new Flight(
+                origin.getText(),
+                destination.getText(),
+                LocalDate.parse( departure.getText()),
+                designatedAirplane.getText(),
+                Integer.parseInt(availableSeats.getText()));
+        FlightDao fDao = new FlightDao();
+        fDao.createFlight(f);
+    }
+
+    private void clearFields () {
+        flightOriginField.setText("");
+        flightDestinationField.setText("");
+        flightDepartureField.setText("");
+        flightDesignatedAirplaneField.setText("");
+        flightAvaliableSeatsField.setText("");
+    }
+
+    private boolean notEmptyField (TextField toTest) {
+        if (toTest.getText().isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+}
